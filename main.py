00a1f94f87341fabe24a4ad5c388a382b7d0f349
ironfist-code <ironfist.github@gmail.com>
2025-10-28 19:45:36 +0200

    Finnished course

--- main.py
+++ main.py
@@ -2,23 +2,64 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 
 # imports constants to main.py
 from constants import *
 
+# imports player class to main.py
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
+from shot import Shot
+
+
 def main():
     pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    player = Player((SCREEN_WIDTH / 2), SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0, 0, 0))
+        dt = clock.tick(60) / 1000.0
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+                    
+
         pygame.display.flip()
-
-
+        
 if __name__ == "__main__":
     main()
